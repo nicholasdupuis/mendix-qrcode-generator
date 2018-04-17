f972c9c874ecba6be76abf91c1d7516a04859e27
Nicholas Dupuis <nicholas.dupuis@ge.com>
2018-04-17 09:34:14 -0500

    Initial Commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const qrcode = require('qrcode-terminal');
+const ip = require('ip');
+
+// Gives an IP Address + port number and page
+const ipAddress = ip.address() + ':3001/index.html';
+
+// Warn user
+console.log('Generating QR Code for address: ' + ipAddress);
+console.log('Ensure you are forwarding port 3001 to 8080 in your VM');
+
+// This object will be turned into a QR code for the Mendix App
+const scannableObject = {
+  "url": ipAddress,
+  "offline": true
+};
+
+// Generate the QR code
+qrcode.generate(JSON.stringify(scannableObject));
